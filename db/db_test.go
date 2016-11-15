7400bf824c1903197bbd44db6ae6872c57b71263
Sebastien Cogneau <sebastien.cogneau@gmail.com>
2016-11-15 13:30:33 +0100

    Cleaning unnecessary code.

--- db/db_test.go
+++ db/db_test.go
@@ -1,7 +1,7 @@
 package db
 
 import (
-	"errors"
+	"database/sql"
 	"fmt"
 	"testing"
 )
@@ -14,14 +14,10 @@ func TestReadFromDb(t *testing.T) {
 
 	var selectFnC = func(c Connection, input ...interface{}) (interface{}, error) {
 
-		if sqlC, ok := c.(sqlConnection); ok {
-			var resultInt int
-			r := sqlC.QueryRow("SELECT 1;")
-			r.Scan(&resultInt)
-			return resultInt, nil
-		}
-
-		return nil, errors.New("La connection doit être une connection SQL")
+		var resultInt int
+		r := c.QueryRow("SELECT 1;")
+		r.Scan(&resultInt)
+		return resultInt, nil
 	}
 
 	r, err := c.doWithoutTransaction(selectFnC, 1, 2)
@@ -43,15 +39,11 @@ func TestReadFromDbWithParameters(t *testing.T) {
 
 	var selectFnC = func(c Connection, input ...interface{}) (interface{}, error) {
 
-		if sqlC, ok := c.(sqlConnection); ok {
-			var resultInt int
-			num := 1
-			r := sqlC.QueryRow("SELECT count(*) from people where id=$1;", int(num))
-			r.Scan(&resultInt)
-			return resultInt, nil
-		}
-
-		return nil, errors.New("La connection doit être une connection SQL")
+		var resultInt int
+		num := 1
+		r := c.QueryRow("SELECT count(*) from people where id=$1;", int(num))
+		r.Scan(&resultInt)
+		return resultInt, nil
 	}
 
 	r, err := c.doWithoutTransaction(selectFnC, 1, 2)
@@ -63,4 +55,30 @@ func TestReadFromDbWithParameters(t *testing.T) {
 		t.Errorf("SELECT 1 should return 1, got %d\n", intValue)
 	}
 
+}
+
+func TestReadWithTransaction(t *testing.T) {
+	c, err := NewSQLConnection("seb", "seb", "tracker_test")
+	if err != nil {
+		t.Error(err)
+	}
+
+	var selectFnC = func(tx *sql.Tx, input ...interface{}) (interface{}, error) {
+
+		var resultInt int
+		num := 1
+		r := tx.QueryRow("SELECT count(*) from people where id=$1;", int(num))
+		r.Scan(&resultInt)
+		return resultInt, nil
+
+	}
+
+	r, err := c.doInTransaction(selectFnC, 1, 2)
+	if err != nil {
+		fmt.Println("Error ", err)
+	}
+	if r != 1 {
+		intValue, _ := r.(int)
+		t.Errorf("SELECT 1 should return 1, got %d\n", intValue)
+	}
 }
