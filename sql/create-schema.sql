ce20c8a12146eabb98b929ad41d6d957ecdab757
Sebastien Cogneau <sebastien.cogneau@gmail.com>
2016-12-23 13:26:38 +0100

    Deploying f/heroku

--- sql/create-schema.sql
+++ sql/create-schema.sql
@@ -1,4 +1,6 @@
+CREATE SEQUENCE people_id_seq;
+
 CREATE TABLE people (
-  id int CONSTRAINT people_primary_key PRIMARY KEY,
+  id int CONSTRAINT people_primary_key PRIMARY KEY DEFAULT nextval('people_id_seq'),
   nom text NOT NULL,
   prenom text NOT NULL);
