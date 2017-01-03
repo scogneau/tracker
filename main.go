0ae17f94d5ca4846d86ae8f0cb8e3682e7813f0f
Sebastien Cogneau <sebastien.cogneau@gmail.com>
2017-01-03 13:19:50 +0100

    Adding landing page.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -16,6 +17,21 @@ func main() {
 	path := flag.String("c", "conf/tracker.conf", "Path of configuration file")
 	flag.Parse()
 	configuration.InitFromPath(*path)
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		var path string
+		if request.RequestURI == "/" {
+			path = "static/index.html"
+		} else {
+			path = "static/" + request.RequestURI
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			log.Printf("Error reading index.html %s", err)
+		}
+		io.Copy(writer, f)
+	})
 	http.HandleFunc("/people/", rest.HandlePeopleRead)
 	fmt.Println("Starting tracker application")
 
